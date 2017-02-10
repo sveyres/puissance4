5610e9219f7f2b6c41af9d1ba3469c936914f91d
Sylvie <sylvie.veyres@gmail.com>
2017-02-10 12:04:49 +0100

    reste à faire le changement de pion au second clic

--- js/script.js
+++ js/script.js
@@ -1,12 +1,11 @@
 var boardElement = document.getElementById("board");
 var gridElement = document.createElement("table");
-var grid = [["","","","","",""],
-            ["","","","","",""],
-            ["","","","","",""],
-            ["","","","","",""],
-            ["","","","","",""],
-            ["","","","","",""],
-            ["","","","","",""]];
+var grid = [["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""],
+            ["","","","","","",""]];
 
 for (var j = 0; j < 7; j++) {
 // LES COLONNES
@@ -17,6 +16,32 @@ for (var j = 0; j < 7; j++) {
     clicElement.className = "truc";
     clicElement.id = j;
     columnElement.appendChild(clicElement);
+
+    clicElement.addEventListener ("click", function(){
+        var col = this.id;
+        var lig = 5;
+        console.log(lig+"-"+col);
+        while (grid[lig][col]!=="") {
+            lig = lig - 1;
+        }
+        var laCase = document.getElementById("case"+lig+"-"+col)
+        laCase.className = "red";
+        grid[lig][col]="R";
+
+    });
+    clicElement.addEventListener ("click", function(){
+        var col = this.id;
+        var lig = 5;
+        console.log(lig+"-"+col);
+        while (grid[lig][col]!=="") {
+            lig = lig - 1;
+        }
+        var laCase = document.getElementById("case"+lig+"-"+col)
+        laCase.className = "yellow";
+        grid[lig][col]="Y";
+
+    });
+
     for (var i = 0; i < 6; i++) {
 // LES LIGNES
         var caseElement = document.createElement("div");
@@ -24,34 +49,30 @@ for (var j = 0; j < 7; j++) {
         caseElement.id = "case"+i+"-"+j;
         columnElement.appendChild(caseElement);
 
-        clicElement.addEventListener ("click", function(){
-            while (grid[i][j] == "") {
-                i = grid.length-1;
-                if () {
-                    grid[i][j] = "Y";
-                    caseElement.className = "yellow";
-                } else {
-                    i--;
-                }
-
-
-
-                                // if(grid[i][this.id]==""){
-                //     caseElement.className ="red";
-
-            //
-            // for (var k = grid.length-1; k > 0; k--) {
-            //     }
-
-                // if(grid[i][j] == "R") {
-                //     caseElement.className ="red";
-                // }
-                // else if(grid[i][j] == "Y"){
-                //     caseElement.className ="yellow";
-                // }
-            }
-
-        });
-
+        //
+        // if(grid[i][j]=="Y"){
+        //     caseElement.className = "yellow";
+        // }else if (grid[i][j]=="R") {
+        //     caseElement.className = "red";
+        //
+        // }
     }
 }
+
+
+
+
+
+// if(grid[i][this.id]==""){
+//     caseElement.className ="red";
+
+//
+// for (var k = grid.length-1; k > 0; k--) {
+//     }
+
+// if(grid[i][j] == "R") {
+//     caseElement.className ="red";
+// }
+// else if(grid[i][j] == "Y"){
+//     caseElement.className ="yellow";
+// }
